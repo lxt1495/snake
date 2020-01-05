9a5519805b4064c8c0609b6f7caa87a9bd88a07b
lxt1495 <lxt1495@gmail.com>
2020-01-05 14:26:36 +0700

    touchend

--- snake.js
+++ snake.js
@@ -784,9 +784,9 @@ function angryKeydown(e){
             document.body.removeEventListener('keydown',checkPress,false)
         }
     },false)
-    window.addEventListener('touchstart',function checkTouch(){
+    window.addEventListener('touchend',function checkTouch(){
         clearInterval(pressIndex)
-        window.removeEventListener('touchstart',checkTouch,false)
+        window.removeEventListener('touchend',checkTouch,false)
     },false)
     if(main.offsetTop===canvasTop){return}
     Object.values(square).forEach(x=>x.style.top=(x.offsetTop-20)+'px')
